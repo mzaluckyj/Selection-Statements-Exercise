0787f527f2ede793e70a6d356ec6133973364e41
Michael <m.zaluckyj@gmail.com>
2022-11-23 15:41:22 -0500

    exercise 1 complete

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Favorite number
+
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            int userInput;
+
+            do
+
+            {
+
+                Console.WriteLine("Try to guess my favorite number");
+
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too low");
+                }
+
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too high");
+                }
+
+                else
+                {
+                    Console.WriteLine("Nevermind"); //Correct! exercise statement "nevermind"
+                }
+
+            } while (userInput != favNumber);
         }
     }
 }
